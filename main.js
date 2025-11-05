a70a7dfd7104e71f971364a4659b724cdce084bf
Leon Nordell <leon.nordell04@gmail.com>
2025-11-05 17:51:45 +0100

    working todo list app

--- main.js
+++ main.js
@@ -4,9 +4,41 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", btnPress);
 
-function btnPress() {
+function btnPress(event) {
   event.preventDefault(); 
-  let item = document.createElement("p");
-  item.textContent = "hello";
-  list.appendChild(item);
+
+  // get input
+  text = input.value;
+  if (text.length === 0) {
+    return;
+  }
+  input.value = "";
+
+  // list item and button
+  const listItem = document.createElement("li");
+  const deleteButton = document.createElement("button");
+
+  // setting text content
+  listItem.textContent = text;
+  deleteButton.textContent = "delete"
+
+  // structuring elements
+  listItem.appendChild(deleteButton);
+  list.appendChild(listItem);
+
+  // eventlistener
+  deleteButton.addEventListener("click", deleteItem);
+
+  // move focus to input field
+  input.focus()
 }
+
+function deleteItem(event) {
+  // refers to button that was clicked
+  const clicked = event.target;
+  // gets parent element of button, which is the <li> element
+  const itemToDelete = clicked.parentElement;
+  itemToDelete.remove();
+}
+
+
