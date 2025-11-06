9721296b96175815bdff0ed7ed0ccc81f7120b83
Leon Nordell <leon.nordell04@gmail.com>
2025-11-06 16:39:43 +0100

    using localstorage to save list items

--- main.js
+++ main.js
@@ -4,16 +4,7 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", btnPress);
 
-function btnPress(event) {
-  event.preventDefault(); 
-
-  // get input
-  text = input.value;
-  if (text.length === 0) {
-    return;
-  }
-  input.value = "";
-
+function createItem(text) {
   // list item and button
   const listItem = document.createElement("li");
   const deleteButton = document.createElement("button");
@@ -28,9 +19,24 @@ function btnPress(event) {
 
   // eventlistener
   deleteButton.addEventListener("click", deleteItem);
+}
+
+function btnPress(event) {
+  event.preventDefault(); 
+
+  // get input
+  const text = input.value;
+  if (text.length === 0) {
+    return;
+  }
+  input.value = "";
+
+  createItem(text)
 
   // move focus to input field
   input.focus()
+
+  saveList();
 }
 
 function deleteItem(event) {
@@ -39,6 +45,45 @@ function deleteItem(event) {
   // gets parent element of button, which is the <li> element
   const itemToDelete = clicked.parentElement;
   itemToDelete.remove();
+
+  saveList()
 }
+
+function saveList() {
+  // gets all list items in ul
+  const items = list.querySelectorAll("li");
+
+  // text content into array (not delete button's x)
+  const listArray = Array.from(items).map(item => {
+    return item.textContent.slice(0, -1);
+  });
+
+  // converts js array to JSON string 
+  localStorage.setItem("todoListItems", JSON.stringify(listArray));
+}
+
+function loadList() {
+  // gets data from browser local storage
+  const savedData = localStorage.getItem("todoListItems");
+
+  // checks if there is any data
+  if (!savedData) {
+    return;
+  }
+
+  // converting JSON string to array
+  const listArray = JSON.parse(savedData);
+
+  // make the list items again
+  listArray.forEach(text => createItem(text));
+}
+
+// Loads list if there is items in local storage
+loadList();
+
+
+
+
+
 
 
